42a524c502b752ce63e55e928e1949b3d51c1b94
Luke Hedger <luke.hedger@gmail.com>
2015-05-27 17:32:53 +0100

    bundle

--- public/js/app.js
+++ public/js/app.js
@@ -16807,31 +16807,31 @@ module.exports = {
             clue: "Soho was once the Earl of Leicester’s hunting ground (Soohoo being a hunting call) and you can still see the old boundary markers. But what were they? Find one on Kingly Street, Berwick Street, Rupert Street or Newman Street.",
             letter: 6,
             answer: "Blue Posts",
-            hint: ""
+            hint: "Good name for a pub"
         },
         {
             clue: "Near to a holy patch of grass you’ll find a rather inanimate conversation on an Australian street - who is it with?",
             letter: 7,
             answer: "Oscar Wilde",
-            hint: ""
+            hint: "The holiness comes from Martin and the Aussie from Adelaide"
         },
         {
             clue: "Time for a treat - on Broadwick Street there’s a shop selling circles with holes for angry settlements.",
             letter: 9,
             answer: "Crosstown",
-            hint: ""
+            hint: "Homer Simpson would love it here - doh"
         },
         {
-            clue: "How many...? Things in a painting @ NPG",
+            clue: "Go to the National Portrait Gallery and head for 38A. You'll find a man sleeping. Take the number of teams he played for from the number of years he lead England - what are you left with?",
             letter: 1,
             answer: "2",
-            hint: ""
+            hint: "Even Becks could do that sum!"
         },
         {
             clue: "Some may say this hound/water-fowl cross-breed could be the best of its kind in London. Experts have refuted these claims.",
             letter: 11,
             answer: "Dog and Duck",
-            hint: ""
+            hint: "Ask Alice"
         },
         {
             clue: "Take a little trip to Spain with Bradley (via Hanway Street) and find song number *.",
@@ -16843,19 +16843,19 @@ module.exports = {
             clue: "Wash your hands outside this fabled shop where Lexington meets Beak.",
             letter: 1,
             answer: "Aesop",
-            hint: ""
+            hint: "Very expensive soap"
         },
         {
             clue: "What sits atop the Toucan's beak on the pavement of Carlisle Street?",
             letter: 3,
             answer: "Guinness",
-            hint: ""
+            hint: "Good source of iron"
         },
         {
-            clue: "The £1m mod has a plaque on Carnaby Street - but what colour is it?",
+            clue: "If Eminem had a theme park this is what it'd be called. Once you're there, head for postcode MM8. Who is crossing the road second?",
             letter: 2,
             answer: "Green",
-            hint: ""
+            hint: "Eminem World"
         },
         {
             clue: "Search Regent Street for a celestial passage",
@@ -16867,7 +16867,7 @@ module.exports = {
             clue: "What happens in Wardour Street stays in Wardour Street - take a gamble and head to number 89. Win the most for a prize!",
             letter: 6,
             answer: "Las Vegas",
-            hint: "Most famous place on Earth to gamble?"
+            hint: "Its like the Hangover Part 1 all over again..."
         },
         {
             clue: "Can you spot a member of the Night's Watch in the Soho Mural?",
